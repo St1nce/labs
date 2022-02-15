58216b4f16f471354cbab44efc1ee36e4bfb75d3
Александр Никитин <alexsndrnktn975@gmail.com>
2022-02-16 01:02:09 +0700

    До

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,20 +8,22 @@ struct product
 	char data[20];
 	float price;
 	int count, expiration;
-}milk;
+};
 
 void data_input()
 {
 	char name[20];
 	char data[20];
-	printf("Введите данные о товарах в формате\nНазвание, цена, количество, дата поступления, срок годности\nВвод:");
+	product milk;
+	printf("Введите данные о товарах в формате:\nНазвание, цена, количество, дата поступления, срок годности\nВвод: ");
 	scanf("%s %f %d %s %d", &name, &milk.price, &milk.count, &data, &milk.expiration);
-	milk.name = strcpy(milk.name, name);
-	milk.data = strcpy(milk.data, data);
-	printf("%s %f %d %s %")
+	strcpy(milk.name, name);
+	strcpy(milk.data, data);
+	printf("%s %d %d %s %d", &milk.name, &milk.price, &milk.count, &milk.data, &milk.expiration);
 }
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
+	data_input();
 }
