93ebdccf0dee694b4cb4706e57a055702c78ae2c
Александр Никитин <alexsndrnktn975@gmail.com>
2022-02-16 00:46:41 +0700

    Добавление функции

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,4 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <locale.h>
 #include <stdio.h>
 #include <string.h> 
@@ -8,25 +6,22 @@ struct product
 {
 	char name[20];
 	char data[20];
-	int price, count, expiration;
+	float price;
+	int count, expiration;
 }milk;
+
+void data_input()
+{
+	char name[20];
+	char data[20];
+	printf("Введите данные о товарах в формате\nНазвание, цена, количество, дата поступления, срок годности\nВвод:");
+	scanf("%s %f %d %s %d", &name, &milk.price, &milk.count, &data, &milk.expiration);
+	milk.name = strcpy(milk.name, name);
+	milk.data = strcpy(milk.data, data);
+	printf("%s %f %d %s %")
+}
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	 char name[20] = "";
-	 milk.count = 10;
-	strcpy(milk.name, name);
-	printf("%d %s", milk.count, milk.name);
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
